4ae13ddfd281fb2a0fc3eab3e2421c730c9687bd
GG79 <midlifegamingcrisis@gmail.com>
2023-03-17 08:26:52 +0200

    added police stash in evidence

--- config.lua
+++ config.lua
@@ -3,16 +3,16 @@ Config = {}
 Config.PoliceOpen = true -- Can Police open all stashes
 
 Config.Stashes = {
-    -- ["geminimansion"] = {
-    --     stashName = "geminimansion",
-    --     coords = vector3(-1730.5, 358.6, 88.73), 
-    --     requirecid = true,
-    --     jobrequired = false,
-    --     gangrequired = false,
-    --     gang = "",
-    --     job = "",
-    --     cid = {"MAH44286"},  
-    --     stashSize = 1250000,
-    --     stashSlots = 125, 
-    -- },
+     ["police-evidence"] = {
+         stashName = "Evidence",
+         coords = vector3(473.45, -990.71, 26.18), 
+         requirecid = false,
+         jobrequired = true,
+         gangrequired = false,
+         gang = "",
+         job = "police",
+         cid = "",  
+         stashSize = 1250000,
+         stashSlots = 125, 
+     },
 }
